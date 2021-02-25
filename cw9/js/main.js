0ff8a0b0e4fe5980159e38f51a5b1b38e0211cc8
krisman98 <sanskarmaharjan6@gmail.com>
2021-02-25 09:05:42 +0545

    new exercise added

---
+++ cw9/js/main.js
@@ -0,0 +1,43 @@
+console.log('Hello');
+//alert('Hello this is Sanskar');
+
+//Use two forward slash for singls comments
+/*
+Use two forward slash with star in between for multipal
+comments
+*/
+
+//Variables
+var a = 'apple';
+console.log('pwd:' + ' ' + a);
+
+var b = 45.5001;
+console.log(b);
+
+var c = 45;
+console.log(c + 1);
+
+/*Manipulate DOM with Javascript:is just a fancy way of 
+saying change HTML  with Javascript*/
+//var age = prompt('What is your age?');
+//document.getElementById('text').innerHTML = age;
+
+//Numbers in JS
+var num = 10;
+
+//Increament
+//num = num + 1;
+//num++;
+//num +=10;
+//console.log(num);
+
+//decreament
+//num = num - 1;
+// num --;
+// num -= 10;
+//console.log(num);
+
+//
+Divide / Multiply * Remainder %
+    console.log(num / 2);
+console.log(nun * 3);
